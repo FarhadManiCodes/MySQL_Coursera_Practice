3bbe58dc2cd700e57d48d9f3ea5f5e7f6074abbf
Farhad Mani <frhdmani@gmail.com>
2021-09-05 15:37:25 +0100

    SOLVE Ex 2

--- Dillard/week3_Ex_2.sql
+++ Dillard/week3_Ex_2.sql
@@ -1,81 +1,54 @@
 -- Set the DataBase to ua_dillards;
-
 DATABASE ua_dillards;
-
--- EXample of Group by
-
-SELECT sku, COUNT(sku), AVG(retail), AVG(cost)
+-- Exercise 2: 
+-- (b) Which stores are common to all four tables? 327 stores
+SELECT store
+FROM ua_dillards.strinfo
+INTERSECT
+SELECT store
+FROM ua_dillards.trnsact
+INTERSECT
+SELECT store
+FROM ua_dillards.skstinfo
+INTERSECT
+SELECT store
+FROM ua_dillards.store_msa;
+-- or unique to specific tables? --91
+--Unique to strinfo
+SELECT strinfo.store AS srtinfo_u
+FROM strinfo
+    LEFT JOIN store_msa ON store_msa.store = strinfo.store
+    LEFT JOIN skstinfo ON skstinfo.store = strinfo.store
+    LEFT JOIN trnsact ON trnsact.store = strinfo.store
+WHERE trnsact.store IS NULL
+    AND skstinfo.store IS NULL
+    AND store_msa.store IS NULL;
+--Unique to store_msa
+SELECT store_msa.store AS store_msa_u
+FROM store_msa
+    LEFT JOIN strinfo ON strinfo.store = store_msa.store
+    LEFT JOIN skstinfo ON skstinfo.store = store_msa.store
+    LEFT JOIN trnsact ON trnsact.store = store_msa.store
+WHERE trnsact.store IS NULL
+    AND skstinfo.store IS NULL
+    AND strinfo.store IS NULL;
+--Unique to skstinfo -no data
+SELECT skstinfo.store AS skstinfo_u
 FROM skstinfo
-GROUP BY sku
-
--- Exercise 1: (a) Use COUNT and DISTINCT to determine how many distinct skus there are in pairs of the
--- skuinfo, skstinfo, and trnsact tables. Which skus are common to pairs of tables,
--- or unique to specific tables?
-SELECT COUNT(DISTINCT skuinfo.sku), COUNT(DISTINCT skstinfo.sku), COUNT(DISTINCT trnsact.sku)
-
-FROM skuinfo 
-LEFT JOIN skstinfo 
-ON skuinfo.sku = skstinfo.sku
-LEFT JOIN trnsact
-ON skuinfo.sku = trnsact.sku;
-
--- Number of distinct sku in each tables
--- skuinfo
-SELECT COUNT(DISTINCT sku)
-
-FROM skuinfo;
-
--- skstinfo
-SELECT COUNT(DISTINCT sku)
-
-FROM skstinfo;
-
--- trnsact
-SELECT COUNT(DISTINCT sku)
-
-FROM trnsact;
-
--- when trnsact is null
-SELECT COUNT(DISTINCT skuinfo.sku), COUNT(DISTINCT skstinfo.sku), COUNT(trnsact.sku)
-
-FROM skuinfo 
-LEFT JOIN trnsact
-ON skuinfo.sku = trnsact.sku
-LEFT JOIN skstinfo 
-ON skuinfo.sku = skstinfo.sku
-
-WHERE trnsact.sku IS NULL;
-
--- When skstinfo is null
-SELECT COUNT(DISTINCT skuinfo.sku), COUNT(DISTINCT skstinfo.sku), COUNT(DISTINCT trnsact.sku)
-
-FROM skuinfo 
-LEFT JOIN trnsact
-ON skuinfo.sku = trnsact.sku
-LEFT JOIN skstinfo 
-ON skuinfo.sku = skstinfo.sku
-
-WHERE skstinfo.sku IS NULL;
-
--- HARD LEFT JOIN
-SELECT COUNT(DISTINCT skuinfo.sku), COUNT(DISTINCT skstinfo.sku), COUNT(DISTINCT trnsact.sku)
-
-FROM skuinfo 
-LEFT JOIN trnsact
-ON skuinfo.sku = trnsact.sku
-
-LEFT JOIN skstinfo 
-ON trnsact.sku = skstinfo.sku;
-
--- OTHER side
--- HARD LEFT JOIN
-SELECT COUNT(DISTINCT skuinfo.sku), COUNT(DISTINCT skstinfo.sku), COUNT(DISTINCT trnsact.sku)
-
-FROM skuinfo 
-
-LEFT JOIN skstinfo 
-ON skuinfo.sku = skstinfo.sku;
-
-LEFT JOIN trnsact
-ON skstinfo.sku = trnsact.sku
-
+    LEFT JOIN strinfo ON strinfo.store = skstinfo.store
+    LEFT JOIN store_msa ON store_msa.store = skstinfo.store
+    LEFT JOIN trnsact ON trnsact.store = skstinfo.store
+    AND trnsact.sku = skstinfo.sku
+WHERE trnsact.store IS NULL
+    AND store_msa.store IS NULL
+    AND strinfo.store IS NULL;
+--Unique to trnsact -no data
+SELECT trnsact.store AS skstinfo_u
+FROM trnsact
+    LEFT JOIN strinfo ON strinfo.store = trnsact.store
+    LEFT JOIN store_msa ON store_msa.store = trnsact.store
+    LEFT JOIN skstinfo ON skstinfo.store = trnsact.store
+    AND trnsact.sku = skstinfo.sku
+WHERE trnsact.store IS NULL
+    AND store_msa.store IS NULL
+    AND strinfo.store IS NULL;
