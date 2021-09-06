e060ee7132ba12e89f20fe4ee9a4e24a4154cde2
Farhad Mani <frhdmani@gmail.com>
2021-09-06 15:29:37 +0100

    answer question 5

--- Dillard/Graded_Quiz_3.sql
+++ Dillard/Graded_Quiz_3.sql
@@ -1,11 +1,32 @@
 -- Set the DataBase to ua_dillards;
 DATABASE ua_dillards;
-
 -- Question 3
 -- On what day was Dillard’s income based on total sum of purchases the greatest
-SELECT TOP 10 SUM(amt) AS income, saledate 
+SELECT TOP 10 SUM(amt) AS income,
+    saledate
 FROM trnsact
 WHERE stype = 'P'
 GROUP BY saledate
-
 ORDER BY income DESC;
+-- Question 4
+-- What is the deptdesc of the departments that have the top 3 greatest
+-- numbers of skus from the skuinfo table associated with them?  
+SELECT TOP 3 deptinfo.deptdesc AS deptdesc,
+    COUNT(skuinfo.sku) as sku_number
+FROM skuinfo
+    JOIN deptinfo ON deptinfo.dept = skuinfo.dept
+GROUP BY deptdesc
+ORDER BY sku_number DESC;
+
+-- Question 5
+--   Which table contains the most distinct sku numbers?
+
+-- skuinfo
+SELECT COUNT(DISTINCT sku) as skuinfo
+FROM skuinfo;
+-- skstinfo
+SELECT COUNT(DISTINCT sku) as skstinfo
+FROM skstinfo;
+-- trnsact
+SELECT COUNT(DISTINCT sku) as trnsact
+FROM trnsact;
