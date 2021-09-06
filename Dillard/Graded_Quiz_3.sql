e4af06e0ffc164b5e7a9b4b0601fabd43bc2cd1a
Farhad Mani <frhdmani@gmail.com>
2021-09-06 15:34:01 +0100

    answer question 7

--- Dillard/Graded_Quiz_3.sql
+++ Dillard/Graded_Quiz_3.sql
@@ -30,3 +30,15 @@ FROM skstinfo;
 -- trnsact
 SELECT COUNT(DISTINCT sku) as trnsact
 FROM trnsact;
+
+-- Question 7
+-- What is the average amount of profit Dillard’s made per day?  
+SELECT  SUM(trnsact.amt-trnsact.quantity*skstinfo.cost) AS sum_profit,
+        count(trnsact.amt) AS number_of_transactions,
+        count(distinct trnsact.saledate) AS number_of_days,
+        sum_profit/number_of_days as profit_per_day
+
+FROM trnsact
+    INNER JOIN skstinfo ON trnsact.store = skstinfo.store
+    AND trnsact.sku = skstinfo.sku
+WHERE trnsact.stype = 'P'; --only purchase
