664b2a352f38a2f216d4eca9fd891e94308dd863
Farhad Mani <frhdmani@gmail.com>
2021-10-03 18:16:11 +0100

    Add  exercise 2 of week5

--- Dillard/week5/EX.sql
+++ Dillard/week5/EX.sql
@@ -13,3 +13,29 @@ GROUP BY
     year_n,month_num
 ORDER BY 
     year_n,month_num;
+
+
+-- Exercise 2. Use a CASE statement within an aggregate function
+-- to determine which sku had the greatest total sales during 
+-- the combined summer months of June, July, and August.
+SELECT
+    SKU,
+    SUM(CASE WHEN EXTRACT(MONTH from saledate) = 6
+             THEN sprice
+        END) AS total_sale_june,
+    SUM(CASE WHEN EXTRACT(MONTH from saledate) = 7
+             THEN sprice
+        END) AS total_sale_july,
+    SUM(CASE WHEN EXTRACT(MONTH from saledate) = 8
+             THEN sprice
+        END) AS total_sale_aug,
+    total_sale_june + 
+    total_sale_july +
+    total_sale_aug AS total_sale_summer
+    
+FROM 
+    TRNSACT
+WHERE
+    STYPE = 'P'
+GROUP BY sku
+ORDER BY total_sale_summer DESC;
