c2f106359a70530f1b34cd99ac8da4623ffdf331
Farhad Mani <frhdmani@gmail.com>
2021-10-04 09:00:42 +0100

    Add  exercise 4 of week5

--- Dillard/week5/EX.sql
+++ Dillard/week5/EX.sql
@@ -55,3 +55,28 @@ GROUP BY
 ORDER BY 
     number_of_days;
 
+
+-- Exercise 4. What is the average daily revenue for each store/month/year combination in
+-- the database? Calculate this by dividing the total revenue for a group by the number of
+-- sales days available in the transaction table for that group.
+SELECT 
+    store, 
+	EXTRACT(MONTH FROM saledate) || EXTRACT(YEAR FROM saledate) AS monthYear,
+	SUM(amt) AS sumRevenue,
+	COUNT(DISTINCT saledate) AS numDays,
+	sumRevenue / numDays   AS avgRevenue,
+	CASE
+		WHEN EXTRACT(MONTH FROM saledate) = 8 AND EXTRACT(YEAR FROM saledate) = 2005
+		THEN 'exclude'
+		ELSE 'include'
+	END AS useDate
+FROM 
+    trnsact
+WHERE 
+    stype = 'P' AND useDate = 'include'
+GROUP BY 
+    monthYear, store, useDate
+HAVING 
+    numDays > 19
+ORDER BY
+    avgRevenue;
